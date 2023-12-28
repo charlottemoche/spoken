deeba7f20565a3067d3696caf8b133a1e0d85bec
Charlotte Moche <charlottebmoche@gmail.com>
2023-12-28 12:15:28 -0500

    fix

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,5 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
@@ -12,4 +7,4 @@ const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 });
 
-export default config;
+module.exports = config;
